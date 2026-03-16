33cf1d71676930e8343d267161bf9c8bb276534f
Gabriele <gabrieletuozzo@gmail.com>
2026-03-16 12:25:12 +0100

    fix(clean_and_reimport_data_db.sh): update MongoDB container name and improve data import commands

--- WebApp/backend/clean_and_reimport_data_db.sh
+++ WebApp/backend/clean_and_reimport_data_db.sh
@@ -1,7 +1,11 @@
-docker stop mongodb
-docker rm mongodb
+docker stop checloud-mongodb
+docker rm checloud-mongodb
 rm -rf mongo_data
 mkdir mongo_data
-docker-compose up -d
-cp ./CHe_cloud_data.json ./mongo_data
-docker exec -it mongodb mongoimport --db mydatabase --collection CHe_cloud_data --file ./mongo_data/CHe_cloud_data.json --jsonArray --upsert
+docker compose up -d
+docker cp ./CHe_cloud_data.json checloud-mongodb:/CHe_cloud_data.json
+docker exec checloud-mongodb mongoimport \
+  --db mydatabase \
+  --collection CHe_cloud_data \
+  --file /CHe_cloud_data.json \
+  --jsonArray --upsert
